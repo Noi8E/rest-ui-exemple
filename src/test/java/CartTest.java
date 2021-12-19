45ff0f0d96ce0e3d7985eb17f35d13a090c7d177
Noi8E <ftretyakov@kosmostyt.ru>
2021-12-19 14:07:20 +0300

    done

--- src/test/java/CartTest.java
+++ src/test/java/CartTest.java
@@ -1,63 +1,69 @@
+import com.codeborne.selenide.Condition;
+import io.restassured.response.Response;
+import org.hamcrest.core.IsNot;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Cookie;
 
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 
 public class CartTest extends TestBase {
 
     @Test
     @DisplayName("Можем добавить товар в корзину без куки в запросе")
     void canAddItemToCartWOCookie() {
-                given()
-                        .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                        .body("addtocart_14.EnteredQuantity=1")
-                        .cookie(HARD_COOKIE)
-                        .when()
-                        .post(baseUrl +"/addproducttocart/details/14/1")
-                        .then()
-                        .statusCode(200)
-                        .body("success", is(true))
-                        .body("message", is("The product has been added to your <a href=\"/cart\">shopping cart</a>"))
-                        .body("updatetopcartsectionhtml", is("(1)"))
-                        .extract().response();
-    }
-    @Test
-    @DisplayName("Можем добавить товар в корзину с сохранением сессии 1 --> 2")
-    void canAddItemToCartWithCookie() {
-        String cookie =
-                given()
-                        .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                        .formParam("Email", "dfff@dd.dd")
-                        .formParam("Password", "9u334fj")
-                        .when()
-                        .post(baseUrl + "/login")
-                        .then()
-                        .statusCode(200)
-                        .extract()
-                        .cookie("NOPCOMMERCE.AUTH");
-         open(baseUrl + "/Themes/DefaultClean/Content/images/logo.png");
-
-         getWebDriver().manage().addCookie(new Cookie("Nop.customer", cookie));
-
-
-
         given()
                 .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                .body("addtocart_14.EnteredQuantity=1")
-                .cookie(cookie)
+                .body("product_attribute_72_5_18=53&product_attribute_72_6_19=54&product_attribute_72_3_20=57&addtocart_72.EnteredQuantity=1")
                 .when()
-                .post(baseUrl +"/addproducttocart/details/14/1")
+                .post(baseUrl + "/addproducttocart/details/72/1")
                 .then()
                 .statusCode(200)
                 .body("success", is(true))
                 .body("message", is("The product has been added to your <a href=\"/cart\">shopping cart</a>"))
                 .body("updatetopcartsectionhtml", is("(1)"))
                 .extract().response();
+    }
+
+
+    @Test
+    @DisplayName("Добавляем товар в корзину с куки в запросе, проверяем что кол-во товара != 1")
+    void canAddItemToCartWithCookie() {
+
+        given()
+                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                .body("product_attribute_72_5_18=53&product_attribute_72_6_19=54&product_attribute_72_3_20=57&addtocart_72.EnteredQuantity=1")
+                .cookie("Nop.customer" + HARD_COOKIE)
+                .when()
+                .post(baseUrl + "/addproducttocart/details/72/1")
+                .then()
+                .statusCode(200)
+                .body("success", is(true))
+                .body("message", is("The product has been added to your <a href=\"/cart\">shopping cart</a>"))
+                .body("updatetopcartsectionhtml", not("(1)"));
+
+
+    }
+
+
+    @Test
+    @DisplayName("UI-проверка на кол-во товара в корзине ")
+    void checkCartItemsValue() {
+        //Открываем сессию браузера
+        open(baseUrl + "/Themes/DefaultClean/Content/images/logo.png");
+        //Подкладываем токен
+        getWebDriver().manage().addCookie(new Cookie("Nop.customer", HARD_COOKIE));
+        //Открываем корзину
+        open(baseUrl + "/cart");
+        //Проверяем что счетчик айтемов !=1
+        $("span.cart-qty").shouldNotHave(Condition.value("1"));
+
 
     }
 
--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,6 +1,5 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
-import org.openqa.selenium.Cookie;
 
 import static com.codeborne.selenide.Selenide.open;
 import static io.restassured.RestAssured.given;
@@ -9,12 +8,31 @@ public class TestBase {
 
     public final String USER_EMAIL = "mail@test.com";
     public final String USER_PASSWORD = "password";
-    public final String HARD_COOKIE = "6bb46985-623e-4125-8f2c-ebc34feea1e4";
+    public final String HARD_COOKIE = "a578b8a0-72b1-4a15-91e8-65255bc54583";
     public static final String baseUrl = "http://demowebshop.tricentis.com";
+
+
+    //todo разобраться с получением куки на старте тестов!
+//    public static final String COOKIE() {
+//        String myCookie =
+//                given()
+//                        .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+//                        .when()
+//                        .get(baseUrl)
+//                        .then()
+//                        .statusCode(200)
+//                        .extract().cookie("Nop.customer");
+//        System.out.println("Куки для теста = " + myCookie);
+//        return myCookie;
+//    }
 
     @BeforeAll
     static void setUp() {
+
         Configuration.browserSize = "600x600";
+
+
     }
+
 
 }
